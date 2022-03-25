af6553cbbbb7998264bf4497d8e2eee31f493226
bayskelt <rinognom@gmail.com>
2022-03-25 15:55:24 +0200

    create

---
+++ main.py
@@ -0,0 +1,17 @@
+# task_1
+print('task_1')
+p = 6
+x = 6
+while x < 30:
+    x += 3
+    p += x
+print(f'p = {p}')
+
+# task_2
+print('\ntask_2')
+k = 9
+y = 9
+while y < 39:
+    y += 3
+    k *= y
+print(f'k = {k}')
